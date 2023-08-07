b745817d838aabe128da0207acefe5067ac8fe6e
htn9sys <hatsune.927@gmail.com>
2023-08-07 12:25:30 +0900

    retry

--- shiritori.rb
+++ shiritori.rb
@@ -5,17 +5,18 @@ n.times do
     words << gets.chomp
 end
 
-success = true
+a = 0
 result = []
-(1...n).each do |i|
+(1..n-1).each do |i|
     result << words[i][0]
+    a += 1
     unless words[i-1][-1] == words[i][0]
         print words[i-1][-1]," ",words[i][0]
-        success = false
+        a = 0
         break
     end
 end
 
-if success
-    puts "Yes"
+unless a == 0
+    puts 'Yes'
 end
